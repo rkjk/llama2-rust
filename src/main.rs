2d2f967a9794098724b016ee6fee3ff3db73a244
Raghav Kuppan <raghav.kuppan@outlook.com>
2023-08-05 17:15:32 +0530

    Add tokenizer and code to read

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::cmp::max;
 use std::fs::read;
+use std::ptr;
 use std::rc::Rc;
 use std::ptr::slice_from_raw_parts;
 use matrixmultiply::sgemm;
@@ -6,7 +8,7 @@ use matrixmultiply::sgemm;
 #[derive(Debug)]
 pub struct Config {
     pub dim: i32, // transformer dimension// transformer dimension
-    pub hidden_fim: i32, // for ffn layers
+    pub hidden_dim: i32, // for ffn layers
     pub n_layers: i32, // number of layers
     pub n_heads: i32, // number of query heads
     pub n_kv_heads: i32, // number of key/value heads (can be < query heads because of multiquery)
@@ -19,7 +21,7 @@ impl Config {
         let ref_data = box_data.as_ref();
         Config {
             dim: ref_data[0],
-            hidden_fim: ref_data[1],
+            hidden_dim: ref_data[1],
             n_layers: ref_data[2],
             n_heads: ref_data[3],
             n_kv_heads: ref_data[4],
@@ -56,7 +58,7 @@ struct TransformerWeights {
 impl TransformerWeights {
     pub fn new(bytes: &[u8], config: &Config, shared_weights: bool) -> TransformerWeights {
         let dim: usize = config.dim as usize;
-        let hidden_dim = config.hidden_fim as usize;
+        let hidden_dim = config.hidden_dim as usize;
         let n_layers = config.n_layers as usize;
         let n_heads = config.n_heads as usize;
         let vocab_size = config.vocab_size as usize;
@@ -150,6 +152,81 @@ impl TransformerWeights {
     }
 }
 
+pub struct RunState {
+    x: Box<[f32]>,
+    // activation at current time stamp (dim,)
+    xb: Box<[f32]>,
+    // same, but inside a residual branch (dim,)
+    xb2: Box<[f32]>,
+    // an additional buffer just for convenience (dim,)
+    hb: Box<[f32]>,
+    // buffer for hidden dimension in the ffn (hidden_dim,)
+    hb2: Box<[f32]>,
+    // buffer for hidden dimension in the ffn (hidden_dim,)
+    q: Box<[f32]>,
+    // query (dim,)
+    k: Box<[f32]>,
+    // key (dim,)
+    v: Box<[f32]>,
+    // value (dim,)
+    att: Box<[f32]>,
+    // buffer for scores/attention values (n_heads, seq_len)
+    logits: Box<[f32]>,
+    // output logits
+
+    // kv cache
+    key_cache: Box<[f32]>,
+    // (layer, seq_len, dim)
+    value_cache: Box<[f32]>, // (layer, seq_len, dim)}
+}
+
+impl RunState {
+    pub fn new(config: &Config) -> RunState {
+        RunState {
+            x: vec![0.0; config.dim as usize].into_boxed_slice(),
+            xb: vec![0.0; config.dim as usize].into_boxed_slice(),
+            xb2: vec![0.0; config.dim as usize].into_boxed_slice(),
+            hb: vec![0.0; config.hidden_dim as usize].into_boxed_slice(),
+            hb2: vec![0.0; config.hidden_dim as usize].into_boxed_slice(),
+            q: vec![0.0; config.dim as usize].into_boxed_slice(),
+            k: vec![0.0; config.dim as usize].into_boxed_slice(),
+            v: vec![0.0; config.dim as usize].into_boxed_slice(),
+            att: vec![0.0; (config.n_heads * config.seq_len) as usize].into_boxed_slice(),
+            logits: vec![0.0; config.vocab_size as usize].into_boxed_slice(),
+            key_cache: vec![0.0; (config.n_layers * config.seq_len * config.dim) as usize].into_boxed_slice(),
+            value_cache: vec![0.0; (config.n_layers * config.seq_len * config.dim) as usize].into_boxed_slice(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Token {
+    score: f32,
+    token: String
+}
+
+impl Token {
+    pub fn new(score: f32, token: String) -> Token {
+        Token {
+            score,
+            token
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Tokenizer {
+    toks: Box<[Token]>
+}
+
+impl Tokenizer {
+    pub fn new(toks: Vec<Token>) -> Tokenizer {
+        Tokenizer {
+            toks: toks.into_boxed_slice()
+        }
+    }
+}
+
 fn accum(a: &mut [f32], b: &[f32]) {
     assert!(a.len() == b.len(), "a and b should be equal length slices");
     for i in 0..a.len() {
@@ -203,6 +280,7 @@ fn bytes_to_box<T: Clone>(bytes: &[u8]) -> Result<Box<[T]>, String> {
         return Err(format!("Size mismatch, bytearray size {}, type size {}", bytes.len(), type_size));
     }
     let num_elements = bytes.len() / type_size;
+    //println!("num_elements: {}", num_elements);
     let mut data = Vec::with_capacity(num_elements);
     let ptr = bytes.as_ptr() as *const T;
     let slice = unsafe { &*slice_from_raw_parts(ptr, num_elements)};
@@ -210,17 +288,53 @@ fn bytes_to_box<T: Clone>(bytes: &[u8]) -> Result<Box<[T]>, String> {
     Ok(data.into_boxed_slice())
 }
 
+fn unaligned_bytes_to_box<T: Clone>(bytes: &[u8]) -> Result<Box<[T]>, String> {
+    let type_size = std::mem::size_of::<T>();
+    if bytes.len() % type_size != 0 {
+        return Err(format!("Size mismatch, bytearray size {}, type size {}", bytes.len(), type_size));
+    }
+    let num_elements = bytes.len() / type_size;
+    let mut data = Vec::with_capacity(num_elements);
+    let ptr = bytes.as_ptr() as *const T;
+    unsafe {
+        for _ in 0..num_elements {
+            data.push(ptr::read_unaligned(ptr));
+        }
+    }
+    Ok(data.into_boxed_slice())
+}
+
 fn read_config(path: &str) -> (Config, TransformerWeights){
-    let vec = read(path).expect("error reading file");
+    let vec = read(path).expect("error reading model");
     let config_size = std::mem::size_of::<Config>();
     let config_data: Box<[i32]> = bytes_to_box(&vec[0..config_size]).unwrap();
     let config = Config::new(config_data);
     let shared_weights = config.vocab_size > 0;
     config.vocab_size - config.vocab_size.abs();
     let transformer_weights = TransformerWeights::new(&vec[config_size..], &config, shared_weights);
-    println!("Config: {:?}", config);
-    println!("TransformerWeights size: {}", std::mem::size_of::<TransformerWeights>());
     (config, transformer_weights)
+}
+
+fn read_tokenizer(path: &str, vocab_size: i32) -> (u32, Tokenizer) {
+    let f32_size = std::mem::size_of::<f32>();
+    let u32_size = std::mem::size_of::<u32>();
+    let vec = read(path).expect("error reading tokenizer");
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut offset: usize = 0; // byte offset
+    let max_token_size: u32 = bytes_to_box::<u32>(&vec[offset..offset + u32_size]).unwrap()[0];
+    offset += u32_size;
+
+    for i in 0..vocab_size as usize {
+        let token_score: f32 = unaligned_bytes_to_box::<f32>(&vec[offset..offset + f32_size]).unwrap()[0];
+        offset += f32_size;
+        let num_chars: u32 = unaligned_bytes_to_box::<u32>(&vec[offset..offset + u32_size]).unwrap()[0];
+        offset += u32_size;
+        let str = std::str::from_utf8(&vec[offset..offset + num_chars as usize]).unwrap();
+        let token = Token::new(token_score, str.to_owned());
+        tokens.push(token);
+        offset += num_chars as usize;
+    }
+    (max_token_size, Tokenizer::new(tokens))
 }
 
 fn main() {
@@ -228,5 +342,10 @@ fn main() {
 
     //TODO: Get from cmd
     let checkpoint_file = "out/model.bin";
+    let tokenizer_file = "tokenizer.bin";
     let (config, transformer_weights) = read_config(checkpoint_file);
+    //read_tokenizer("tokenizer.bin", config.vocab_size);
+    let (max_token_size, tokenizer) = read_tokenizer(tokenizer_file, config.vocab_size);
+    println!("Config: {:?}", config);
+    println!("Max token size: {}", max_token_size);
 }
